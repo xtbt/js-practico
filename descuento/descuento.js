dc27f9f2aa456fb3aa4850918dcf51e2b2e9e336
xtbt <jsanchez.1983@gmail.com>
2021-08-02 07:26:51 -0700

    Promedios finalizado. Pediente analisis salarial

--- descuento/descuento.js
+++ descuento/descuento.js
@@ -1,4 +1,87 @@
 'use strict';
 window.onload = (ev) => {
-    
+    const productos = [
+        {"descripcion": "Bola del dragón de 4 estrellas", "costo": 5550},
+        {"descripcion": "Bastón del maestro Roshi", "costo": 320},
+        {"descripcion": "Arete derecho de Vegetto", "costo": 75350},
+        {"descripcion": "Nube voladora", "costo": 110999},
+        {"descripcion": "Capa de Piccolo", "costo": 49867},
+        {"descripcion": "Huevo de cell", "costo": 150385},
+        {"descripcion": "Maquina del tiempo de Trunks", "costo": 999999},
+        {"descripcion": "Espada de Trunks", "costo": 459345}
+    ];
+    const cupones = [
+        {"codigo": "kame-hame-ha", "descuento": 15},
+        {"codigo": "sheng-long", "descuento": 30},
+        {"codigo": "gogeta", "descuento": 45},
+        {"codigo": "kakarotto", "descuento": 60}
+    ];
+
+    const divisa = new Intl.NumberFormat(undefined, {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2
+    });
+
+    const producto1 = productos[Math.floor(Math.random() * productos.length)];
+    const costoProducto1 = producto1.costo;
+    document.getElementById('nombreProducto1').innerText = 'Producto: ' + producto1.descripcion;
+    document.getElementById('numCostoProducto1').value = divisa.format(costoProducto1);
+
+    const producto2 = productos[Math.floor(Math.random() * productos.length)];
+    const costoProducto2 = producto2.costo;
+    document.getElementById('nombreProducto2').innerText = 'Producto: ' + producto2.descripcion;
+    document.getElementById('numCostoProducto2').value = divisa.format(costoProducto2);
+
+    // Acciones para forma de descuento fijo ----------------------------------
+    const btnCalcularDescuentoFijo = document.getElementById('btnCalcularDescuentoFijo');
+    btnCalcularDescuentoFijo.addEventListener('click', function() {
+        const descuentoProducto1 = "" != document.getElementById('numDescuentoProducto1').value ? Number(document.getElementById('numDescuentoProducto1').value) : 0;
+        const numDescuentoSobreCosto1 = document.getElementById('numDescuentoSobreCosto1');
+        const numTotalConDescuento1 = document.getElementById('numTotalConDescuento1');
+        const msjAvisoDescuentoFijo = document.getElementById('msjAvisoDescuentoFijo')
+        if (0 != descuentoProducto1) {
+            numDescuentoSobreCosto1.value = divisa.format(descuentoSobreCosto(costoProducto1, descuentoProducto1));
+            numTotalConDescuento1.value = divisa.format(totalConDescuento(costoProducto1, descuentoProducto1));
+            msjAvisoDescuentoFijo.innerText = '';
+        }
+        else {
+            numDescuentoSobreCosto1.value = '';
+            numTotalConDescuento1.value = '';
+            msjAvisoDescuentoFijo.innerText = 'Se debe especificar un número entre 1 y 100';
+        }
+    })
+
+    const btnCalcularDescuentoCupon = document.getElementById('btnCalcularDescuentoCupon');
+    btnCalcularDescuentoCupon.addEventListener('click', function() {
+        const cuponDescuento = document.getElementById('txtCuponDescuento2').value;
+        const numDescuentoSobreCosto2 = document.getElementById('numDescuentoSobreCosto2');
+        const numTotalConDescuento2 = document.getElementById('numTotalConDescuento2');
+        const msjInfoCupon = document.getElementById('msjInfoCupon');
+        const msjAvisoDescuentoCupon = document.getElementById('msjAvisoDescuentoCupon');
+        const cupon = cupones.find(objetoActual => {
+            return objetoActual.codigo == cuponDescuento;
+        });
+        if (undefined != cupon) {
+            msjInfoCupon.innerText = 'Descuento aplicado: ' + cupon.descuento + '%';
+            numDescuentoSobreCosto2.value = divisa.format(descuentoSobreCosto(costoProducto2, cupon.descuento));
+            numTotalConDescuento2.value = divisa.format(totalConDescuento(costoProducto2, cupon.descuento));
+            msjAvisoDescuentoCupon.innerText = '';
+        }
+        else {
+            msjInfoCupon.innerText = '';
+            numDescuentoSobreCosto2.value = '';
+            numTotalConDescuento2.value = '';
+            msjAvisoDescuentoCupon.innerText = 'Introduce un código válido';
+        };
+    })
+
+    function descuentoSobreCosto (costo, descuento) {
+        return costo * descuento / 100;
+    }
+
+    function totalConDescuento (costo, descuento) {
+        return costo - descuentoSobreCosto(costo, descuento);
+    }
 }
---
+++ promedio/promedio.js
@@ -0,0 +1,62 @@
+'use strict';
+window.onload = (ev) => {
+    let arrayElementos = [];
+    const selElementos = document.getElementById('selElementos');
+    selElementos.setAttribute('size', '10');
+    
+    for (let i = 0; i < 20; i ++) {
+        const valor = Math.floor(Math.random() * 30);
+        const elemento = document.createElement('option');
+        elemento.value = valor;
+        elemento.innerText = valor;
+        selElementos.appendChild(elemento);
+        arrayElementos.push(valor);
+    };
+
+    const btnCalcularPromedio = document.getElementById('btnCalcularPromedio');
+    btnCalcularPromedio.addEventListener('click', () => {
+        const numPromedio = document.getElementById('numPromedio');
+        numPromedio.value = calcularPromedio(arrayElementos);
+    });
+
+    const btnCalcularMediana = document.getElementById('btnCalcularMediana');
+    btnCalcularMediana.addEventListener('click', () => {
+        const numMediana = document.getElementById('numMediana');
+        numMediana.value = calcularMediana(arrayElementos);
+    });
+
+    const btnCalcularModa = document.getElementById('btnCalcularModa');
+    btnCalcularModa.addEventListener('click', () => {
+        const numModa = document.getElementById('numModa');
+        numModa.value = calcularModa (arrayElementos);
+    });
+
+    function calcularPromedio (arrayElementos) {
+        const acumulado = arrayElementos.reduce((valorAcumulado, valorActual) => valorAcumulado + valorActual, 0);
+        return acumulado / arrayElementos.length;
+    };
+
+    function calcularMediana (arrayElementos) {
+        arrayElementos.sort((valor1, valor2) => valor1 - valor2);
+        if (0 != arrayElementos.length % 2) {
+            return arrayElementos[Math.floor(arrayElementos.length / 2)];
+        }
+        else {
+            let arrayElementosCentrales = [];
+            arrayElementosCentrales.push(arrayElementos[Math.floor(arrayElementos.length / 2 - 1)]);
+            arrayElementosCentrales.push(arrayElementos[Math.floor(arrayElementos.length / 2)]);
+            return calcularPromedio(arrayElementosCentrales);
+        };
+    };
+
+    function calcularModa (arrayElementos) {
+        let objetoIncidencias = {};
+        arrayElementos.map((elementoActual) => {
+            objetoIncidencias[elementoActual] ? objetoIncidencias[elementoActual] ++ : objetoIncidencias[elementoActual] = 1;
+        });
+        console.log(objetoIncidencias);
+        const arrayAcumulados = Object.entries(objetoIncidencias).sort((valor1, valor2) => valor1[1] - valor2[1]);
+        console.log(arrayAcumulados);
+        return arrayAcumulados[arrayAcumulados.length-1][0] + ' Incidencias: ' + arrayAcumulados[arrayAcumulados.length-1][1];
+    };
+}
